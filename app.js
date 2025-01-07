d9219386afba4afe4bea118836b505352117006a
Mongudd <eugen991215@gmail.com>
2025-01-07 19:10:30 +0900

    Painting Board - Mouse painting

--- app.js
+++ app.js
@@ -3,32 +3,26 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 ctx.lineWidth = 2;
+let isPainting = false;
 
-const colors = [
-  "#ff3838",
-  "#ffb8b8",
-  "#c56cf0",
-  "#ff9f1a",
-  "#fff200",
-  "#32ff7e",
-  "#7efff5",
-];
-
-const randomX = Math.random() * 800;
-const randomY = Math.random() * 800;
-console.log(randomX, randomY);
-
-function onClick(event) {
-  ctx.beginPath();
-  const randomX = Math.random() * 800;
-  const randomY = Math.random() * 800;
-  console.log(randomX, randomY);
-  ctx.moveTo(randomX, randomY);
-  const color = colors[Math.floor(Math.random() * colors.length)];
-  ctx.strokeStyle = color;
-  ctx.lineTo(event.offsetX, event.offsetY);
-  ctx.stroke();
+function onMove(event) {
+  if (isPainting) {
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+    return;
+  }
+  ctx.moveTo(event.offsetX, event.offsetY);
 }
 
-canvas.addEventListener("mousemove", onClick);
-canvas.addEventListener("mousemove", onMouseMoveCounter);
+function startPainting() {
+  isPainting = true;
+}
+
+function canclePainting() {
+  isPainting = false;
+}
+
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", startPainting);
+canvas.addEventListener("mouseup", canclePainting);
+canvas.addEventListener("mouseleave", canclePainting);
